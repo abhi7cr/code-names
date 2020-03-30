e736d7454e9f401477417e692ef47ce47f6bd080
Abhishek Raj <abhi7cronaldo@gmail.com>
2020-03-30 12:00:23 -0700

    disable animation for now

--- codenames.js
+++ codenames.js
@@ -467,16 +467,16 @@ function drawGrid (words) {
 									loadGame()
 							}
 							if (teamBelongingTo === TEAM_STYLE.NEUTRAL || teamBelongingTo != currentTeamEnum[currentTeam]) {
-								const shakeAnimation = [
-									{transform: 'rotate(2deg)'},
-									{transform: 'rotate(-2deg)'},
-									{transform: 'rotate(1deg)'},
-									{transform: 'rotate(-1deg)'}
-								]
-								wordDiv.animate(shakeAnimation, {
-									duration: 1000,
-									easing: 'ease-in-out'
-								})
+// 								const shakeAnimation = [
+// 									{transform: 'rotate(2deg)'},
+// 									{transform: 'rotate(-2deg)'},
+// 									{transform: 'rotate(1deg)'},
+// 									{transform: 'rotate(-1deg)'}
+// 								]
+// 								wordDiv.animate(shakeAnimation, {
+// 									duration: 1000,
+// 									easing: 'ease-in-out'
+// 								})
 								endTurn()
 							}
 							
